eae72a95537432eee9f9738bb67ee92c287f1a86
thoul <huaxiaxiaoyao@gmail.com>
2018-08-02 19:47:01 +0800

    user one

--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.hashers import make_password,check_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import render, redirect
 # Create your views here.
 from user.models import User
@@ -16,13 +16,27 @@ def register(request):
             # 设置用户登录状态
             request.session['uid'] = user.id
             request.session['nickname'] = user.nickname
-            return redirect('/user/info/?uid')
+            return redirect('/user/info/')
         else:
             return render(request, 'register.html', {'error': form.errors})
     return render(request, 'register.html')
 
 
 def login(request):
+    if request.method == "POST":
+        nickname = request.POST.get("nickname")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(nickname=nickname)
+        except User.DoesNotExist:
+            return render(request, "login.html", {"error": '用户不存在，请再次确认后输入'})
+        if check_password(password, user.password):
+            request.session['uid'] = user.id
+            request.session['nickname'] = user.nickname
+            return redirect('/user/info/')
+        else:
+            return render(request, "login.html", {'error': '密码错误'})
     return render(request, 'login.html', {})
 
 
